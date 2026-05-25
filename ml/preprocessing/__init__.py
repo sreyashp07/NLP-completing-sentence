94c2331b8857249192353ed7a21e0c6bd31a5425
sreyashp07 <sreyash1990@gmail.com>
2026-05-25 12:29:58 +0530

    refactor: update preprocessing package init to expose all modules via clean public API

--- ml/preprocessing/__init__.py
+++ ml/preprocessing/__init__.py
@@ -1,3 +1,21 @@
-"""NLP preprocessing pipeline package."""
+"""
+NLP Preprocessing package.
+
+Exposes all preprocessing utilities for clean imports.
+"""
 from ml.preprocessing.text_cleaner import TextCleaner, extract_keywords
-__all__ = ["TextCleaner", "extract_keywords"]
+from ml.preprocessing.text_normalizer import normalize_text, fix_misspellings
+from ml.preprocessing.sentiment_analyzer import analyze_sentiment
+from ml.preprocessing.language_detector import detect_language
+from ml.preprocessing.text_stats import get_text_length_stats, get_class_balance
+
+__all__ = [
+    "TextCleaner",
+    "extract_keywords",
+    "normalize_text",
+    "fix_misspellings",
+    "analyze_sentiment",
+    "detect_language",
+    "get_text_length_stats",
+    "get_class_balance",
+]
